e484e6aa77c31016f5adac28c0a2de7a12fd9068
pastthepixels <pastthescreen@gmail.com>
2022-08-27 15:27:00 -0600

    Made main.py executable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 from lmms_midi import *
 import sys
 
 try:
     parse_xml(str(sys.argv[1])).compile_export()
 except (IndexError, FileNotFoundError) as err:
-    print("lmms-midi\nUSAGE:\n\tmain.py FILENAME.mmp")
+    print("lmms-midi\n=========\nUSAGE:\n\tmain.py FILENAME.mmp")
