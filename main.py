22135e982ec72b9ac993f7f9168b9b2112b6a8de
PastThePixels <pastthescreen@gmail.com>
2022-09-24 15:05:05 -0600

    Add the ability to export multiple files at once

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from lmms_midi import *
 import sys
 
 try:
-    parse_xml(str(sys.argv[1])).compile_export()
+    for i in range(1, len(sys.argv)):
+    	print(sys.argv[i])
+    	parse_xml(str(sys.argv[i])).compile_export()
 except (IndexError, FileNotFoundError) as err:
     print("lmms-midi\n=========\nUSAGE:\n\tmain.py FILENAME.mmp")
