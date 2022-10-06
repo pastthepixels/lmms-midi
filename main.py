7bfa9a60986e2671dea808ac6338abf9673e9256
PastThePixels <pastthescreen@gmail.com>
2022-10-05 20:52:21 -0600

    Add flags to main.py

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python3
 from lmms_midi import *
-import sys
+import sys, os
 
-try:
+debug = False
+files = []
+
+def export_all():
+    for path in files:
+    	print(path)
+    	parse_xml(path).compile_export()
+
+def find_flags():
+    global debug
+    global files
     for i in range(1, len(sys.argv)):
-    	print(sys.argv[i])
-    	parse_xml(str(sys.argv[i])).compile_export()
-except (IndexError, FileNotFoundError) as err:
-    print("lmms-midi\n=========\nUSAGE:\n\tmain.py FILENAME.mmp")
+        match sys.argv[i]:
+            case "--debug":
+                debug = True
+
+            case "--help":
+                help()
+                os._exit(0)
+
+            case _:
+                files.append(str(sys.argv[i]))
+
+def help():
+    print("lmms-midi\n=========\nUSAGE:\n\tmain.py FILENAME.mmp\nFLAGS:")
+    print("\t--debug   --> Shows IndexError and FileNotFoundError errors instead of assuming you did something wrong")
+    print("\t--help    --> Shows this screen")
+
+find_flags()
+
+if debug:
+    export_all()
+else:
+    try:
+        export_all()
+    except (IndexError, FileNotFoundError) as err:
+        help()
